5d6125e14bc56d2abca35fd433d9ec141ecea4da
Ronak Shah <ronakshah725@gmail.com>
2015-09-30 01:14:06 -0500

    q2 completed

---
+++ js/q2.js
@@ -0,0 +1,96 @@
+		// $(document).ready(function(){
+		//     $("#hide").click(function(){
+		//         $("#courses").hide(500);
+		//     });
+		//     $("#show").click(function(){
+		//         $("hide").show();
+		//     });
+		// });
+
+
+
+// function toggle(id) {
+//   var e = document.getElementById(id);
+//   if(e.style.display == 'none') {
+
+//       '#toggle_block'.show();
+//   } else {
+//       e.style.display = 'none';
+//       '#toggle_block'.hide(500);
+//9660
+//   }
+// }
+$(document).ready(function(){
+	$("#ms").hide();
+    $("#hide").click(function(){
+        $("#ms").hide();
+    });
+    $("#show").click(function(){
+        $("#ms").show();
+    });
+});
+
+$(document).ready(function(){
+	$("li.animate").on('mouseenter',function(){
+  $(this).animate({"padding-left": "50px"}, "slow");
+});
+$("li.animate").on('mouseleave',function(){
+  $(this).stop(true).animate({"padding-left": "0px"}, "fast");
+});
+
+});
+
+
+
+$(document).ready(function(){
+	$('#show').click(function(){$('#ms').hide();});
+	$('#hide').click(function(){$('#ms').show();});
+
+    $("#triangle").click(function(){
+    	if($('#triangle').text() == "Show.."){
+
+    		$('#triangle').html("Hide..");
+    		$("#toggle_block").toggle();
+    		
+
+    	}
+    	else{
+		$('#triangle').html("Show..");
+        $("#toggle_block").toggle();
+        
+
+
+   		}	
+
+    });
+    
+});
+
+
+i=0;
+$(document).ready(function(){
+  $("#t1").keypress(function(){
+ $("#s1").text(i+=1);
+  $("#p5").fadeToggle().toggle().fadeToggle();
+ 
+});
+ }); 
+
+$(document).ready(function(){
+  $("#t2").keydown(function(){
+     $( "#t2" ).animate({
+    width:"toggle"}).animate({height:"toggle"}).animate({opacity:0.4});
+  });
+$("#t2").keyup(function(){
+$("#t2").css("background-color","grey");
+
+});
+
+});
+
+
+
+
+
+
+
